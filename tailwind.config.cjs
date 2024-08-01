14bfb54f50abc313dc4d779036c5d697de8a41b8
Luis Puga <luispuga@luisp-2.local>
2024-08-01 19:14:02 -0400

    bug fix - some weird link issue docuemnted in updates.md

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
 				"box-bg": "rgb(var(--color-box))",
 				"box-shadow": "rgb(var(--box-sd))",
 				"box-border": "rgb(var(--box-border))",
-				primary: "#225A96",
+				primary: "#51BBFE",
 				"heading-1": "rgb(var(--heading-1))",
 				"heading-2": "rgb(var(--heading-2))",
 				"heading-3": "rgb(var(--heading-3))",
